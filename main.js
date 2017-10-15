c239dbd50de2eca6ab3d9df253e163a81eb1efd1
Tr3v0r_C <trevor.clarke62@gmail.com>
2017-10-15 12:14:35 -0400

    Moved code around

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 window.onload = setup;
-var particles = [], canvas, c, height = window.innerHeight, width = window.innerWidth, circleColour,
-    colours = ["red", "green", "blue", "orange", "purple", "yellow", "pink"],
+var particles = [],
+    canvas, c, height = window.innerHeight,
+    width = window.innerWidth,
+    circleColour,
     startNum = 40,
     startSize = 15;
 
@@ -12,24 +14,21 @@ function setup() {
     createParticle(20);
     window.setInterval(animate, 10);
 }
-
-window.onresize = function(){
-  height = window.innerHeight;
-  width = window.innerWidth;
-  canvas.height = height;
-  canvas.width = width;
+window.onresize = function() {
+    height = window.innerHeight;
+    width = window.innerWidth;
+    canvas.height = height;
+    canvas.width = width;
 };
 
 function animate() {
-    c.clearRect(0, 0, width, height);
+    clear();
     updateParticles();
-
     c.lineWidth = 3;
     c.strokeStyle = circleColour;
     c.beginPath();
     c.arc((width / 2) - (startSize / 2), (height / 2) - (startSize / 2), startSize, 0, 2 * Math.PI);
     c.stroke();
-
     for (i = 0; i < particles.length; i++) {
         c.beginPath();
         c.fillStyle = particles[i].c;
@@ -39,16 +38,15 @@ function animate() {
 }
 
 function createParticle(num) {
-    if (num == null)
-        num = 1;
+    if (num == null) num = 1;
     for (i = 0; i < num; i++) {
         var colour = randomColour();
         particles.push({
-             x: (width / 2) - (startSize / 2),
-             y: (height / 2) - (startSize / 2),
-             z: 1,
-            vX:	(Math.random() * 1.5 - .75),
-            vY: Math.random() * 2 -1,
+            x: (width / 2) - (startSize / 2),
+            y: (height / 2) - (startSize / 2),
+            z: 1,
+            vX: (Math.random() * 1.5 - .75),
+            vY: Math.random() * 2 - 1,
             vZ: Math.random() * .02 - .01,
             c: colour
         });
@@ -61,17 +59,13 @@ function updateParticles() {
         var vX = particles[i].vX;
         var vY = particles[i].vY;
         var vZ = particles[i].vZ;
-
         particles[i].vX *= Math.abs(vZ) + 1;
         particles[i].vY *= Math.abs(vZ) + 1;
-
         particles[i].x += vX;
         particles[i].y += vY;
         particles[i].z += vZ;
-
         var pad = particleSize(particles[i].z);
-
-        if (!(particles[i].z > 0 && particles[i].y > 0-pad && particles[i].y < height +pad && particles[i].x > 0-pad && particles[i].x < width+pad)) {
+        if (!(particles[i].z > 0 && particles[i].y > 0 - pad && particles[i].y < height + pad && particles[i].x > 0 - pad && particles[i].x < width + pad)) {
             removeParticle(i);
             createParticle();
         }
@@ -80,10 +74,9 @@ function updateParticles() {
 }
 
 function sortParticles() {
-    particles.sort(
-        function(a, b) {
-            return a.z - b.z;
-        });
+    particles.sort(function(a, b) {
+        return a.z - b.z;
+    });
 }
 
 function removeParticle(i) {
@@ -98,5 +91,9 @@ function randomColour() {
 }
 
 function particleSize(a) {
-        return startSize * (Math.pow(a, 1.5));
+    return startSize * (Math.pow(a, 1.5));
+}
+
+function clear() {
+    c.clearRect(0, 0, width, height);
 }
